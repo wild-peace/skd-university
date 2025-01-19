b5e7ed76a993a663d911ef7686918348ad56f212
wild <2446203522@qq.com>
2025-01-19 23:24:17 +0800

    第二次提交测试

---
+++ 算法/排序/冒泡.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	        int arr[] = {3, 9, -1, 10, 20};
+ 
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+ } 
